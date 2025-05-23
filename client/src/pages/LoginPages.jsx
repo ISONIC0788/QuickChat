3e265310db233f4b92a4afbd4a8430aa2951233f
Ebed meleck <ituzeebedi12@gmail.com>
2025-05-23 17:26:23 +0200

    Login page modified and Pofile page layout added

--- client/src/pages/LoginPages.jsx
+++ client/src/pages/LoginPages.jsx
@@ -8,6 +8,15 @@ function LoginPages() {
   const [password , setPassword] = useState("");
   const [bio , setBio] = useState("");
   const [isDataSubmitted , setIsDataSubmitted] = useState(false);
+
+  const onSubmitHandler = (event) =>{
+      event.preventDefault();
+
+      if(currState === "Sign up" && !isDataSubmitted){
+        setIsDataSubmitted(true);
+         return;
+      }
+  }
   return (
     <div className='min-h-screen bg-cover bg-center flex items-center justify-center gap-8 sm:justify-evenly 
     max-sm:flex-col backdrop-blur-2xl'>
@@ -16,11 +25,13 @@ function LoginPages() {
 
         {/* ---------------right---------------- */}
 
-        <form action="" className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg
+        <form onSubmit={onSubmitHandler} className='border-2 bg-white/8 text-white border-gray-500 p-6 flex flex-col gap-6 rounded-lg
          shadow-lg'>
            <h2 className='font-medium text-2xl flex  justify-between items-center '>
             {currState}
-            <img src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />   
+            { isDataSubmitted &&   
+            <img  onClick={()=> setIsDataSubmitted(false)} src={assets.arrow_icon} alt="" className='w-5 cursor-pointer' />}
+             
             </h2>
             { currState === "Sign up" && !isDataSubmitted && (
                 <input 
@@ -68,7 +79,7 @@ function LoginPages() {
                <div className='flex flex-col gap-2'>
                  {currState === "Sign up " ? (
                     <p className='text-sm text-gray-600'>Already have an account ? <span
-                    onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false);}} 
+                    onClick={()=>{setCurrState("Login"); setIsDataSubmitted(false)}} 
                      className='font-medium text-violet-500 cursor-pointer'>Login Here</span></p>
                  ) : (
                     <p className='text-sm text-gray-600'>Create an account <span 
